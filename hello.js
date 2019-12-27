d8986d3a809befd6df9a058e29ecc91bbb4c875a
ltc <1280096922@qq.con>
2019-12-27 22:56:59 +0800

    js引用其他文件

---
+++ hello.js
@@ -0,0 +1,13 @@
+var hello = function(){
+    console.log("hello world!");
+    //return "hello world!";
+}
+
+var data = {
+    id: 1,
+    name: "ltc",
+    home: "zhangbei"
+}
+
+exports.function1 = hello;
+exports.data1 = data;
---
+++ use_hello.js
@@ -0,0 +1,7 @@
+var use = require("./hello.js");
+
+var ff = use.function1;
+//var back = ff();
+ff();
+//console.log(back);
+console.log(use.data1.name);
